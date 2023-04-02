415d5d6791a0d9f86eded94a014908a954788999
satishhalhalli <satishhalhalli7484@gmail.com>
2023-04-02 16:45:36 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import { Container, Navbar } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+import {Container, Navbar} from 'react-bootstrap';
 
 import Products from './Sharp/Products';
 import CartBtn from './Cart/CartBtn';
 import Cart from './Cart/Cart';
 import { useState } from 'react';
 import CartProvider from './Store/CartProvider';
-import About from './About/About';
-// import Home from './Home';
+import Home from './Home';
+import About from './About';
 
 function App() {
   const [state, setState] = useState(false);
-
   return (
-    <>
+    <Router>
       <CartProvider>
-      <Router>
         <Navbar bg="success" expand="lg">
           <Container>
-            <Navbar.Brand href="#">E-Commerce</Navbar.Brand>
-            <NavLink to="/" className="nav-link">
-              Home
-            </NavLink>
-            <NavLink to="/about" className="nav-link">
-              About
-            </NavLink>
+            <Navbar.Brand href="#"> E-Commerce</Navbar.Brand>
+            <NavLink to="/" exact activeClassName="active">Home</NavLink>
+            <NavLink to="/about" activeClassName="active">About</NavLink>
+            <NavLink to="/products" activeClassName="active">Products</NavLink>
             <CartBtn onClick={() => setState(true)} />
           </Container>
         </Navbar>
 
-        {state && <Cart onClose={() => setState(false)} />}
+        {state &&  <Cart onClose={() => setState(false)} />}
 
-        
-          <Switch>
-            <Route exact path="/" component={Products} />
-            <Route path="/about" component={About} />
-            {/* <Route path="/home" component={Home} /> */}
-          </Switch>
-        </Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/products" component={Products} />
+        </Switch>
       </CartProvider>
-    </>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "../node_modules/react-bootstrap/dist/react-bootstrap"
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-
